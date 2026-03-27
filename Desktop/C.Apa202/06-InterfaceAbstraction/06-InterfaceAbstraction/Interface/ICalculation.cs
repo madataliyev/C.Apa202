fec091e654216772c5dbcc29f7a4b08cde5b7efb
madataliyev <madatma-apa202@code.edu.az>
2026-03-27 21:07:10 +0400

    homework

---
+++ Desktop/C.Apa202/06-InterfaceAbstraction/06-InterfaceAbstraction/Interface/ICalculation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_InterfaceAbstraction.Interface
+{
+    internal  interface ICalculation
+    {
+        void Calculate();
+    }
+}
---
+++ Desktop/C.Apa202/06-InterfaceAbstraction/06-InterfaceAbstraction/Program.cs
@@ -0,0 +1,29 @@
+﻿using _06_InterfaceAbstraction.Interface;
+using _06_InterfaceAbstraction.Models;
+using System;
+using System.Threading.Channels;
+class Proqram:ICalculation
+{
+    public static void Main(string[] args)
+    {
+        Calculation calculator = new Calculation();
+
+        Console.WriteLine("----Calculator a Xos Gelmisiniz !!!----");
+
+        Console.WriteLine("Birinci ededi daxil edin: ");
+        double x=Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Emeliyyati secin (+,-,*,/): ");
+        char symbol= Convert.ToChar(Console.ReadLine());
+
+        Console.WriteLine("Ikinci ededi daxil edin: ");
+        double y=Convert.ToDouble(Console.ReadLine());
+
+        double finished=calculator.Calculate(x,y,symbol);
+        Console.WriteLine("Cavab: "+ finished);
+       
+    }
+
+    public void Calculate() { }
+   
+}
