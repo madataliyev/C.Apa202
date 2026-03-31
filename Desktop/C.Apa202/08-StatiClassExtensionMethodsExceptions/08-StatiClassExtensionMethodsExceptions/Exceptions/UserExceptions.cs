7179bd36c3c1e6d762ba5d685f3c2f4a864a84da
madataliyev <madatma-apa202@code.edu.az>
2026-03-31 13:13:47 +0400

    task

---
+++ Desktop/C.Apa202/08-StatiClassExtensionMethodsExceptions/08-StatiClassExtensionMethodsExceptions/Exceptions/UserExceptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace _08_StatiClassExtensionMethodsExceptions.Exceptions { 
+    public class InvalidUsernameException : Exception
+    {
+        public InvalidUsernameException() : base("Username 3 simvoldan az olmamalıdır!") { }
+        public InvalidUsernameException(string message) : base(message) { }
+    }
+
+    public class InvalidPasswordException : Exception
+    {
+        public InvalidPasswordException() : base("Password 6 simvoldan az olmamalıdır!") { }
+        public InvalidPasswordException(string message) : base(message) { }
+    }
+
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException(string username) : base($"Sistemdə {username} adlı istifadəçi tapılmadı.") { }
+    }
+
+    public class IncorrectPasswordException : Exception
+    {
+        public int AttemptsLeft { get; set; }
+        public IncorrectPasswordException(int left) : base($"Şifrə yanlışdır. Qalan cəhd: {left}")
+        {
+            AttemptsLeft = left;
+        }
+    }
+
+    public class AccountLockedException : Exception
+    {
+        public AccountLockedException() : base("Hesab bloklanıb!") { }
+    }
+} 
---
+++ Desktop/C.Apa202/08-StatiClassExtensionMethodsExceptions/08-StatiClassExtensionMethodsExceptions/Program.cs
@@ -0,0 +1,49 @@
+﻿using _08_StatiClassExtensionMethodsExceptions.Exceptions;
+using ProjectName.Services;
+class Program
+{
+    static void Main()
+    {
+        LoginSystem system = new LoginSystem();
+
+        while (true)
+        {
+            try
+            {
+               
+                Console.Write("Username: ");
+                string name = Console.ReadLine();
+                Console.Write("Password: ");
+                string pass = Console.ReadLine();
+
+                if (system.Login(name, pass)) break;
+            }
+            catch (InvalidUsernameException ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+            catch (InvalidPasswordException ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+            catch (UserNotFoundException ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+                Console.WriteLine("Mövcud userlər: admin, student, teacher");
+            }
+            catch (IncorrectPasswordException ex)
+            {
+                Console.WriteLine("WARNING: " + ex.Message);
+            }
+            catch (AccountLockedException ex)
+            {
+                Console.WriteLine("CRITICAL: " + ex.Message + " Adminlə əlaqə saxlayın.");
+                break;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("UNEXPECTED ERROR: " + ex.Message);
+            }
+        }
+    }
+}
