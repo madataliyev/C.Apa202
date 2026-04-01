0af8e9230a08a958375c0ae9d66c4cb7574486e7
madataliyev <madatma-apa202@code.edu.az>
2026-04-01 08:43:47 +0400

    task

---
+++ Desktop/C.Apa202/09- UpcastingDowncastingExplicitImplicit/09- UpcastingDowncastingExplicitImplicit/Models/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09__UpcastingDowncastingExplicitImplicit.Models
+{
+    //internal abstract class Animal
+    //{
+    //    public int AvgLifeTime { get; set; }
+    //    public string Gender { get; set; }
+       
+
+    //    public virtual void Eat()
+    //    {
+    //        Console.WriteLine("qidalanir");
+    //    }
+    }
+
---
+++ Desktop/C.Apa202/09- UpcastingDowncastingExplicitImplicit/09- UpcastingDowncastingExplicitImplicit/Models/Dog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09__UpcastingDowncastingExplicitImplicit.Models
+{
+    //internal class Dog:Animal
+    //{
+    //    public string Breed { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
